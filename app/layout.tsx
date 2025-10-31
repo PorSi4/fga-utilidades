3199a9ecc862d3c3505abd444ec7525288462b50
PorSi4 <felipeferreira1891@gmail.com>
2025-10-31 14:52:40 -0300

    att 0.05 pixel add (part 3)

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR"> {/* Mude para pt-BR */}
+      <head>
+        {/* VERIFICAÇÃO DO DOMÍNIO FACEBOOK - ADICIONE ESTA LINHA */}
+        <meta name="facebook-domain-verification" content="85y14i635nvd5pgbe99b2n20zcymdj" />
+      </head>
       <body className={`font-sans antialiased`}>
         {children}
         <Analytics />
